0564fb08a8cd6b1b4cd23c1faf7c51d0bfa93896
wuweishan <wuweishan95@foxmail.com>
2021-08-08 11:06:41 +0800

    增加使用AspectJ来处理全局防抖的示例

--- aopdemo/src/main/java/com/example/aopdemo/AopActivity.java
+++ aopdemo/src/main/java/com/example/aopdemo/AopActivity.java
@@ -3,6 +3,10 @@ package com.example.aopdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.Region;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,11 +15,19 @@ import com.example.aopdemo.aop.ClickAction;
 import com.example.aopdemo.aop.LoginCheck;
 
 public class AopActivity extends AppCompatActivity {
-
+    private View btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aop);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 查看反编译后的代码，发现对OnClickListener进行了编译期织入，即加上了AspectUtil里的防抖逻辑
+                Log.e("WWS", "click btnLogin");
+            }
+        });
     }
 
     @ClickAction(name = "登录")
@@ -36,6 +48,5 @@ public class AopActivity extends AppCompatActivity {
     public void coupon(View view) {
         Log.i(Constants.TAG, "coupon");
         startActivity(new Intent(this, OtherActivity.class));
-
     }
 }
